62be4245e4348f739c7d33645cd8ee74bd28542b
Kateriina <blondinkova@mail.ru>
2022-08-21 22:55:05 +0300

    Added two buttons(play/pause)

--- app/src/main/java/space/kate/appblock04/MainActivity.java
+++ app/src/main/java/space/kate/appblock04/MainActivity.java
@@ -2,13 +2,29 @@ package space.kate.appblock04;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    MediaPlayer pianoMusic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        pianoMusic = MediaPlayer.create(this, R.raw.piano);
+
+    }
+
+    public void playMusic(View v){
+        pianoMusic.start();
+    }
+
+    public void pauseMusic(View v){
+        if(pianoMusic.isPlaying()){
+            pianoMusic.pause();
+        }
     }
 }
